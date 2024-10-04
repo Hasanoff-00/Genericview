47fba0e2d18a2fcca24e4965bac45d7d21534d22
customuser <hasanovsuhrob2004@gmail.com>
2024-10-05 03:41:51 +0500

    Generic

---
+++ dashboard/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Faculty, Group, Student, Kafedra, Subject, Teacher
+
+admin.site.register(Faculty)
+admin.site.register(Group)
+admin.site.register(Student)
+admin.site.register(Kafedra)
+admin.site.register(Subject)
+admin.site.register(Teacher)
+
---
+++ dashboard/views.py
@@ -0,0 +1,52 @@
+# dashboard/views.py
+from rest_framework import generics
+from .models import Faculty, Group, Student, Kafedra, Subject, Teacher
+from .serializers import FacultySerializer, GroupSerializer, StudentSerializer, KafedraSerializer, SubjectSerializer, TeacherSerializer
+
+class FacultyList(generics.ListCreateAPIView):
+    queryset = Faculty.objects.all()
+    serializer_class = FacultySerializer
+
+class FacultyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Faculty.objects.all()
+    serializer_class = FacultySerializer
+
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class StudentList(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class KafedraList(generics.ListCreateAPIView):
+    queryset = Kafedra.objects.all()
+    serializer_class = KafedraSerializer
+
+class KafedraDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Kafedra.objects.all()
+    serializer_class = KafedraSerializer
+
+class SubjectList(generics.ListCreateAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SubjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class TeacherList(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
